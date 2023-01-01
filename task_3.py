9431c3491578f0e4aa7bb1f123e9e55b22593b89
Jakimc <88589226+JJakimcz@users.noreply.github.com>
2023-01-01 15:59:15 +0100

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,2 +1,11 @@
 def min_max(data):
-    return 0, 0
+    minimum=data(1)
+    maximum=data(1)
+
+    for l in len(data):
+        if l<minimum:
+            minimum=l
+        if l>maximum:
+            maximum=l
+
+    return minimum, maximum
