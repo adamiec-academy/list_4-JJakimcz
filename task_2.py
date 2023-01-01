3f9eec0ef6093df907bdf57e846b8b7614c8002c
Jakimc <88589226+JJakimcz@users.noreply.github.com>
2023-01-01 15:54:08 +0100

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,2 +1,15 @@
+
 def my_split(text):
-    return [text]
+    s=[]
+    s1=""
+    
+    for l in text:
+        if l != " ":
+            s1+=l
+        elif s1 != "":
+            s.append(s1)
+            s1=""
+    if s1!="":
+        s.append(s1)
+    
+    return s
