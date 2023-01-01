f28d5540608c595d4a8401b87ccb2b7dd4c3537c
Jakimc <88589226+JJakimcz@users.noreply.github.com>
2023-01-01 15:36:56 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,8 @@
 def is_palindrome(text):
-    return False
+       x1 = text.replace(" ", "")
+       x1 = x1.lower()
+       x2 = x1[::-1]
+       if x1==x2:
+        return(True)
+       else:
+        return(False)
